f481c0e0d652745887410a0ffd08aef12e71d718
Nicolas Le Gall <contact@nlegall.fr>
2020-10-26 09:31:49 +0100

    Add CRUD_model

---
+++ CRUD_model/example.php
@@ -0,0 +1,31 @@
+<?php
+
+class Model_Example extends \Model_Crud
+{
+    protected static $_properties = array(
+        'id',
+        'title',
+        'person_id',
+    );
+
+    protected static $_table_name = 'exmaples';
+
+    public static function post_find($result)
+    {
+        if($result !== null)
+        {
+            foreach ($result as $value)
+            {
+                if ( ! empty($value->person_id))
+                {
+                    $person = Model_Person::find_one_by_id($value->person_id)->name;
+                    $value->set(array('person_nom' => $person));
+                }
+            }
+        }
+        
+        // return the result
+        return $result;
+    }
+
+}
