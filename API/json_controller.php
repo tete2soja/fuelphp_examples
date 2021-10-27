f7a25e29c44b36cf67cb087f18370e9c17a55d40
Nicolas Le Gall <contact@nlegall.fr>
2021-10-27 14:26:07 +0200

    Create advance SQL clause

--- API/json_controller.php
+++ API/json_controller.php
@@ -16,4 +16,37 @@ class Controller_Api extends Controller_Rest
         ));
         return $result;
     }
+
+
+    public function get_data()
+    {
+        $a_json = array();
+        $a_json['total'] = Model_Fournisseur::count();
+        if (Input::get('search') != "")
+        {
+            $result = Model_Exemple::find(function ($query)
+            {
+                return $query->select('table.*')
+                             ->join('table2')
+                             ->on('table.id', '=', 'table2.fk_id')
+                             ->where('table.message', 'like', '%'.Input::get('search').'%')
+                             ->or_where('table2.title', 'like', '%'.Input::get('search').'%');
+            });
+            if (isset($result)) {
+                $a_json['total'] = count($result);
+            }
+            else
+                $a_json['total'] = 0;
+        }
+        else
+        {
+            if (Input::get('limit') != "") {
+                $result = Model_Fournisseur::find_by('delete', '0', '=', Input::get('limit'), Input::get('offset'));
+            }
+            else
+                $result = Model_Fournisseur::find_by('delete', '0', '=', 100, 0);
+        }
+        $a_json['rows'] = $result;
+        return $this->response($a_json);
+    }
 }
